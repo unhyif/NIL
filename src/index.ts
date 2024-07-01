7ab79601b17d06aef1b3e37cdb1e346eaa70db06
unhyif <ang21icjh@gmail.com>
2024-07-01 19:32:01 +0900

    feat: initial setup for config, create, update commands

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
-export const add = (a: number, b: number) => {
-  return a + b;
-};
+import { Command } from 'commander';
+import { input } from '@inquirer/prompts';
+import { version } from '../package.json';
 
-console.log(add(1, 2));
+const program = new Command();
+
+program.name('nil');
+program.version(version);
+
+program
+  .command('config')
+  .description('Configure')
+  .action(async () => {
+    const answer = await input({ message: 'Enter anything' });
+    console.log(answer);
+  });
+
+program
+  .command('create')
+  .description('Create')
+  .action(async () => {
+    const answer = await input({ message: 'Enter anything' });
+    console.log(answer);
+  });
+
+program
+  .command('update')
+  .description('Update')
+  .action(async () => {
+    const answer = await input({ message: 'Enter anything' });
+    console.log(answer);
+  });
+
+program.on('command:*', () => {
+  console.error('Invalid command: %s\n', program.args.join(' '));
+  program.help();
+  process.exit(1);
+});
+
+if (process.argv.length === 2) {
+  console.log('Welcome to NIL!\n');
+  program.help();
+  process.exit(1);
+}
+
+program.parse(process.argv);
